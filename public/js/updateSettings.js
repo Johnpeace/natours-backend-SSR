dff82355928de48f6dfeba06dde1608ad8f7b105
Ropo Olatujoye <ropo@nairabox.com>
2021-01-10 14:11:10 +0100

    just testing some ssh fix

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -6,10 +6,14 @@ import { showAlert } from './alerts';
 // type is eithehr 'password' or 'data'
 export const updateSettings = async (data, type) => {
   try {
-    const path = type === 'password' ? 'updateMyPassword' : 'updateMe';
+    const url =
+      type === 'password'
+        ? '/api/v1/users/updateMyPassword'
+        : '/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: `/api/v1/users/${path}`,
+      url,
       data,
     });
 
