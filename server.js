891774bc515a118f13788a1c599a9418ee2b6139
Ropo Olatujoye <ropo@nairabox.com>
2021-03-09 11:56:37 +0100

    add generic db

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ process.on('uncaughtException', (error) => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE_LOCAL;
-// const DB = process.env.DATABASE.replace(
-//   '<PASSWORD>',
-//   process.env.DATABASE_PASSWORD
-// );
+// const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
